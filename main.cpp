3f3b67a3bab0b86efd1e0dad83e2e4fe7bb7d416
ifoxpl3460 <j.jackowski123@gmail.com>
2021-12-20 17:17:54 +0100

    lolololol

--- main.cpp
+++ main.cpp
@@ -1,6 +1,54 @@
 #include <SFML/Graphics.hpp>
 
+// tile class
+class Tile {
+private:
+	sf::Vector2f size;
+public:
+	sf::RectangleShape tile;
+	Tile () {
+		size = sf::Vector2f (100.f, 100.f);
+		tile.setSize (size);
+		tile.setFillColor (sf::Color::White);
+		tile.setOutlineThickness (1.f);
+		tile.setOutlineColor (sf::Color::Red);
+	}
+	void SetPosition (float x, float y) {
+		tile.setPosition (x, y);
+	}
+};
+
 int main () {
-	sf::RenderWindow* win = new sf::RenderWindow (sf::VideoMode (300, 300), "tic-tac-toe");
+	// sfml stuff
+	sf::RenderWindow* win = new sf::RenderWindow (sf::VideoMode (301, 301), "tic-tac-toe");
+	sf::Event* evnt = new sf::Event;
+
+	// temporary tab
+	Tile tiles[9];
+
+	int n = 1, m = 1;
+
+	// setting up position for each tile
+	for (int i = 1; i < 10; i++) {
+		tiles[i-1].SetPosition (n, m);
+		n += 100;
+		if (i % 3 == 0) {
+			n = 1;
+			m += 100;		
+		}
+	}
+
+	// main loop
+	while (win->isOpen ()) {
+		while (win->pollEvent (*evnt)) {
+			if (evnt->type == sf::Event::Closed) win->close ();
+		}
+		win->clear ();
+
+		for (int i = 0; i < 9; i++) {
+			win->draw (tiles[i].tile);
+		}	
+		win->display ();
+	}
 	return 0;
 }
